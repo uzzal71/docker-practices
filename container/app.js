21c5b1b1f365a95dd6e16d0a3c9710710b64a715
uzzal71 <uzzalroy.acm@gmail.com>
2024-02-09 21:05:38 +0600

    first push successfully

--- container/app.js
+++ container/app.js
@@ -11,10 +11,6 @@ app.get("/api", (req, res) => {
   res.send("Welcome to api page");
 });
 
-app.get("/users", (req, res) => {
-  res.send("Welcome to api page");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
